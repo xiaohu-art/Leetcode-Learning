c271237068be109a691f1bfdc2423e96d21b79d5
xiaohu-art <3508577791@qq.com>
2022-10-06 00:21:46 +0800

    归并排序求逆序对数量（左闭右开）

--- 05-10-2022/solution.py
+++ 05-10-2022/solution.py
@@ -15,7 +15,7 @@
 
 class Solution(object):
     def mergesort(self, nums, left, right, snums):
-        if left == right:
+        if  (right - left) == 1 :
             '''
             为了防止列表为空这里递归基的条件可以改成：
             left >= right
@@ -24,36 +24,36 @@ class Solution(object):
 
         mid = (left + right) // 2
         lcnt = self.mergesort(nums, left, mid, snums)
-        rcnt = self.mergesort(nums, mid+1, right, snums)
-        if nums[mid] <= nums[mid+1]:                # 稍微加速归并排序
+        rcnt = self.mergesort(nums, mid, right, snums)
+        if nums[mid-1] <= nums[mid]:                # 稍微加速归并排序
             return lcnt + rcnt
         ccnt = self.merge(nums, left, mid, right, snums)
         return lcnt + rcnt + ccnt
 
     def merge(self, nums, left, mid, right, snums):
-        i, j, pos = left, mid+1, left               # 分别指向左区间，右区间，有序列表
+        i, j, pos = left, mid, left               # 分别指向左区间，右区间，有序列表
         cnt = 0
-        while i <= mid and j <= right:
+        while i < mid and j < right:
             if nums[i] <= nums[j]:
                 snums[pos] = nums[i]
                 i += 1
             else:
                 snums[pos] = nums[j]
                 j += 1
-                cnt += (mid-i+1)
+                cnt += (mid-i)
             pos += 1
                                                     # 拷贝剩余元素
-        while i <= mid:
+        while i < mid:
             snums[pos] = nums[i]
             pos += 1
             i += 1
 
-        while j <= right:
+        while j < right:
             snums[pos] = nums[j]
             pos += 1
             j += 1
 
-        nums[left:right+1] = snums[left:right+1]    # 赋值回原数组
+        nums[left:right] = snums[left:right]    # 赋值回原数组
 
         return cnt
 
@@ -71,13 +71,13 @@ class Solution(object):
         if n <= 1:                                        
             return 0
 
-        cnt = self.mergesort(nums, 0, n-1, snums)         # 区间 左闭右闭
+        cnt = self.mergesort(nums, 0, n, snums)         # 区间 左闭右开
 
         return cnt
 
          
 
-nums = []
+nums = [7, 5, 6, 4]
 sol = Solution()
 print(sol.reversePairs(nums))
 
